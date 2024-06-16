809c276463fd705732cb7bf99257f5c57fe8f206
notBradPitt <msbumblebee1998+github2@gmail.com>
2024-06-16 22:42:52 +0700

    IP Adapter

---
+++ custom_nodes/IP Adapter/__init__.py
@@ -0,0 +1,19 @@
+"""
+ ██▓ ██▓███   ▄▄▄      ▓█████▄  ▄▄▄       ██▓███  ▄▄▄█████▓▓█████  ██▀███
+▓██▒▓██░  ██▒▒████▄    ▒██▀ ██▌▒████▄    ▓██░  ██▒▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒
+▒██▒▓██░ ██▓▒▒██  ▀█▄  ░██   █▌▒██  ▀█▄  ▓██░ ██▓▒▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒
+░██░▒██▄█▓▒ ▒░██▄▄▄▄██ ░▓█▄   ▌░██▄▄▄▄██ ▒██▄█▓▒ ▒░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄
+░██░▒██▒ ░  ░ ▓█   ▓██▒░▒████▓  ▓█   ▓██▒▒██▒ ░  ░  ▒██▒ ░ ░▒████▒░██▓ ▒██▒
+░▓  ▒▓▒░ ░  ░ ▒▒   ▓▒█░ ▒▒▓  ▒  ▒▒   ▓▒█░▒▓▒░ ░  ░  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░
+ ▒ ░░▒ ░       ▒   ▒▒ ░ ░ ▒  ▒   ▒   ▒▒ ░░▒ ░         ░     ░ ░  ░  ░▒ ░ ▒░
+ ▒ ░░░         ░   ▒    ░ ░  ░   ░   ▒   ░░         ░         ░     ░░   ░
+ ░                 ░  ░   ░          ░  ░                     ░  ░   ░
+                        ░
+             · -—+ IPAdapter Plus Extension for QuasarUI +—- ·
+             Brought to you by Matteo "Matt3o/Cubiq" Spinelli
+             https://github.com/cubiq/QuasarUI_IPAdapter_plus/
+"""
+
+from .IPAdapterPlus import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
